3e35308c61dea95aecafeeffdf9de19753451dc9
Fernanda Charbel <fernandacharbel@gmail.com>
2023-10-24 10:57:41 -0300

    Adicionando validações

--- src/routes.js
+++ src/routes.js
@@ -7,12 +7,15 @@ const {
     deleteStudent
 } = require('./controllers/studentController');
 
+const validateRequest = require('./middlewares/validateRequest');
+const { updateStudentSchema, studentSchema } = require('./validations/studentSchema');
+
 const route = express();
 
-route.post('/alunos', registerStudent);
+route.post('/alunos', validateRequest(studentSchema), registerStudent);
 route.get('/alunos', getStudents);
 route.get('/alunos/:id', detailStudent);
-route.put('/alunos/:id', updateStudent);
+route.put('/alunos/:id', validateRequest(updateStudentSchema), updateStudent);
 route.delete('/alunos/:id', deleteStudent);
 
 module.exports = route;
---
+++ src/validations/studentSchema.js
@@ -0,0 +1,58 @@
+const joi = require('joi')
+
+const studentSchema = joi.object({
+    nome: joi.string().required().messages({
+        'any.required': 'O campo nome é obrigatório',
+        'string.empty': 'O campo nome é obrigatório'
+    }),
+    idade: joi.number().integer().min(1).required().messages({
+        'any.required': 'O campo idade é obrigatório',
+        'any.empty': 'O campo idade é obrigatório',
+        'number.min': 'A idade deve ser maior ou igual a 1',
+        'number.base': 'O campo idade deve ser um número'
+    }),
+    nota_primeiro_semestre: joi.number().messages({
+        'number.base': 'O campo nota_primeiro_semestre deve ser um número'
+    }),
+    nota_segundo_semestre: joi.number().messages({
+        'number.base': 'O campo nota_segundo_semestre deve ser um número'
+    }),
+    nome_professor: joi.string().required().messages({
+        'any.required': 'O campo nome_professor é obrigatório',
+        'string.empty': 'O campo nome_professor é obrigatório'
+    }),
+    numero_da_sala: joi.number().integer().min(1).required().messages({
+        'any.required': 'O campo numero_da_sala é obrigatório',
+        'any.empty': 'O campo numero_da_sala é obrigatório',
+        'number.min': 'O numero_da_sala deve ser maior ou igual a 1',
+        'number.base': 'O campo numero_da_sala deve ser um número'
+    })
+});
+
+const updateStudentSchema = joi.object({
+    nome: joi.string().messages({
+        'string.empty': 'O campo nome não pode estar vazio'
+    }),
+    idade: joi.number().integer().min(1).messages({
+        'any.empty': 'O campo idade não pode estar vazio',
+        'number.min': 'A idade deve ser maior ou igual a 1',
+        'number.base': 'O campo idade deve ser um número'
+    }),
+    nota_primeiro_semestre: joi.number().messages({
+        'number.base': 'O campo nota_primeiro_semestre deve ser um número'
+    }),
+    nota_segundo_semestre: joi.number().messages({
+        'number.base': 'O campo nota_segundo_semestre deve ser um número'
+    }),
+    nome_professor: joi.string().messages({
+        'string.empty': 'O campo nome_professor não pode estar vazio'
+    }),
+    numero_da_sala: joi.number().integer().min(1).messages({
+        'any.empty': 'O campo numero_da_sala não pode estar vazio',
+        'number.min': 'O numero_da_sala deve ser maior ou igual a 1',
+        'number.base': 'O campo numero_da_sala deve ser um número'
+    })
+});
+
+
+module.exports = { studentSchema, updateStudentSchema };
