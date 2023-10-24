5f8b64ac554925c27100f3e2d0c304f656b9c1ae
Fernanda Charbel <fernandacharbel@gmail.com>
2023-10-24 11:57:16 -0300

    Corrigindo a ordem do middleware cors

--- src/index.js
+++ src/index.js
@@ -7,9 +7,10 @@ const swaggerFile = require('../swagger/swagger_output.json');
 
 const app = express();
 
+app.use(cors());
+
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(express.json());
-app.use(cors());
 app.use(route);
 
 app.listen(process.env.PORT)
