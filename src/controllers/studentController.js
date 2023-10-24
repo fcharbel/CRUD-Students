c8601c3611b80a9b75766a1f0a2497caeba1516d
Fernanda Charbel <fernandacharbel@gmail.com>
2023-10-24 10:31:42 -0300

    Função para atualizar estudante

--- src/controllers/studentController.js
+++ src/controllers/studentController.js
@@ -32,13 +32,67 @@ const registerStudent = async (req, res) => {
     }
 }
 
+const getStudents = async (req, res) => {
+    try {
+        const allStudents = await knex('alunos');
+        res.status(200).json(allStudents);
 
-const getStudent = (req, res) => {
-    res.status(200).json('Esta  funcionando')
+    } catch (error) {
+        res.status(500).json({ mensagem: 'Erro interno do servidor' });
+    }
 };
+
+const detailStudent = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const student = await knex('alunos').where({ id });
+        res.status(200).json(student);
+    } catch (error) {
+        res.status(500).json({ mensagem: 'Erro interno do servidor' });
+    }
+}
+
+const updateStudent = async (req, res) => {
+    const { id } = req.params;
+    const {
+        nome,
+        idade,
+        nota_primeiro_semestre,
+        nota_segundo_semestre,
+        nome_professor,
+        numero_da_sala
+    } = req.body;
+
+    try {
+        const updatedStudent = await knex('alunos')
+            .where({ id })
+            .update({
+                nome,
+                idade,
+                nota_primeiro_semestre,
+                nota_segundo_semestre,
+                nome_professor,
+                numero_da_sala
+            })
+            .returning("*");
+
+        if (!updatedStudent) {
+            return res.status(400).json({ mensagem: "Erro ao atualizar estudante." });
+        }
+
+        return res
+            .status(200)
+            .json({ mensagem: "Estudante atualizado com sucesso!", estudante_atualizado: updatedStudent });
+
+    } catch (error) {
+        res.status(500).json({ mensagem: 'Erro interno do servidor' });
+    }
+}
 
 
 module.exports = {
     registerStudent,
-    getStudent
+    getStudents,
+    detailStudent,
+    updateStudent
 };
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 const express = require('express');
-const { registerStudent, getStudent } = require('./controllers/studentController');
+const {
+    registerStudent,
+    getStudents,
+    detailStudent,
+    updateStudent
+} = require('./controllers/studentController');
 
 const route = express();
 
 route.post('/alunos', registerStudent);
-route.get('/', getStudent);
+route.get('/alunos', getStudents);
+route.get('/alunos/:id', detailStudent);
+route.put('/alunos/:id', updateStudent)
 
 module.exports = route;
